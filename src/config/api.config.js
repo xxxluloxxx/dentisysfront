2f287dced6a815487cfdb8f9fed55f51944980a0
Paul Arias <paul.arias@bayteq.com>
2025-04-03 11:53:22 -0500

    Se cambia url prod

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -3,7 +3,7 @@ const API_CONFIG = {
     // URLs base para diferentes ambientes
     BASE_URL: {
         development: 'http://localhost:8081',
-        production: 'https://dentisys-zsns.onrender.com', // Cambia esto por tu URL de producción
+        production: 'https://dentisys-production.up.railway.app', // Cambia esto por tu URL de producción
         staging: 'https://staging-api.tudominio.com' // Cambia esto por tu URL de staging
     },
 
