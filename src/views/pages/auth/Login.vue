05f4d5e3e4954597d6bb7190cb701861e205587f
Paul Arias <paul.arias@bayteq.com>
2025-04-25 12:08:21 -0500

    Se cambia el nombre del login

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -49,7 +49,7 @@ const handleLogin = async () => {
                 <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                     <div class="text-center mb-8">
                         <img src="@/assets/logos/logo.svg" alt="Logo DentiSys" class="mb-8 w-16 shrink-0 mx-auto" />
-                        <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Bienvenido a DentiSys</div>
+                        <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Bienvenido a Prisma</div>
                         <span class="text-muted-color font-medium">Inicia sesión para continuar</span>
                     </div>
 
